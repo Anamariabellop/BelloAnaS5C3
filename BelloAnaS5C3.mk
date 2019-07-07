78cf5a07dc7da43bcff42df9af6cfc4c317e58dd
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-06 20:49:09 -0500

    make

--- BelloAnaS5C3.mk
+++ BelloAnaS5C3.mk
@@ -1,9 +1,8 @@
 #makefile
-
 BelloAnaResorte.pdf: datosresorte.dat BelloAna_S5C3_plots.py BelloAna_S5C3_ODES.cpp
 	python BelloAna_S5C3_plots.py
 datosresorte.dat: a.out
 	./a.out
 a.out: BelloAna_S5C3_ODES.cpp
 	g++ BelloAna_S5C3_ODES.cpp
-	
+
