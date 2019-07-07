f77b73350f1fe4396a57fd97ddc5e7eeab208991
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-06 20:48:31 -0500

    plot b

--- BelloAna_S5C3_plots.py
+++ BelloAna_S5C3_plots.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-datos=np.genfromtxt("datosresorte.dat")
+datos=np.genfromtxt("datosresorte.txt")
 t=datos[:,0]
 x=datos[:,1]
+v=datos[:,2]
 
 plt.figure()
-plt.plot(t,x, label='x(t)')
+plt.plot(t,x, label='vel',marker='1')
+#plt.plot(t,v, label='pos')
 plt.xlabel("t")
 plt.ylabel("x")
-plt.ylim(-0.2,2)
-plt.title("Spring and mass")
 plt.legend()
+plt.title("Spring and mass")
 plt.savefig("BelloAnaResorte.pdf")
