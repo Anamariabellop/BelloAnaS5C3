df49866cc87067ce195fd3ce6de21caad3c5a352
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-06 21:05:00 -0500

    posicion en funcion de t

--- BelloAna_S5C3_plots.py
+++ BelloAna_S5C3_plots.py
@@ -7,10 +7,11 @@ x=datos[:,1]
 v=datos[:,2]
 
 plt.figure()
-plt.plot(t,x, label='vel',marker='1')
-#plt.plot(t,v, label='pos')
+plt.plot(t,x,color='purple' ,label='posicion')
+#plt.plot(t,v, label='vel')
 plt.xlabel("t")
 plt.ylabel("x")
+#plt.ylim(-0.15,0.15)
 plt.legend()
-plt.title("Spring and mass")
+plt.title("Posicion en funcion de t")
 plt.savefig("BelloAnaResorte.pdf")
