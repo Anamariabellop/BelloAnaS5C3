88563c365ac5e37c5b87295b1fd89ce8d7ab936d
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-07 21:37:27 -0500

    subplot

--- BelloAna_S5C3_plots.py
+++ BelloAna_S5C3_plots.py
@@ -7,11 +7,19 @@ x=datos[:,1]
 v=datos[:,2]
 
 plt.figure()
+
+plt.subplot(2,1,1)
 plt.plot(t,x,color='purple' ,label='posicion')
-#plt.plot(t,v, label='vel')
 plt.xlabel("t")
 plt.ylabel("x")
-#plt.ylim(-0.15,0.15)
 plt.legend()
 plt.title("Posicion en funcion de t")
+
+plt.subplot(2,1,2)
+plt.plot(t,v,color='c' ,label='vel')
+plt.xlabel("t")
+plt.ylabel("v")
+plt.legend()
+plt.title("Posicion en funcion de t")
+
 plt.savefig("BelloAnaResorte.pdf")
